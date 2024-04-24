eda8576d88648aeb3579e8b892d2bae2b19895e5
RekulakuntaNihanth <167198873+RekulakuntaNihanth@users.noreply.github.com>
2024-04-24 10:59:33 +0530

    Add files via upload

---
+++ python_variables.py
@@ -0,0 +1,55 @@
+#while
+a=5
+while a<10:
+    print(a)
+    a+=1
+    
+a=65
+while a<91:
+    print(chr(a),end=" ")
+    a+=1
+# variable
+tax=float(input("enter tax:"))
+print(tax)
+print(type(tax))
+
+age=int(input("enter age:"))
+print(age)
+print(type(age))
+
+name=str(input("enter name:"))
+print(name)
+print(type(name))
+# variables
+a=b=c=d=55
+print(c)
+x,y,z=10,20.5,'mango'
+print(x)
+
+a1="python is"
+b1=" awsome"
+print(a1+b1)
+
+a2,b2="10","20"
+c2="30"
+d2=a2+b2+c2
+print(d2)
+
+a2=50
+print('my age=',a2)
+# variables
+Apple=10
+apple=20
+app_le=30
+_apple=40
+apples='''mango'''
+'''@apple=120
+$apple=50
+&apple=60'''
+#10apple=70
+app10le=80
+apple20=90
+#app le=100
+APPLE=110
+print(app_le)
+print(apples)
